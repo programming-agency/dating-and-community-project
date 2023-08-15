e4b44213302457ee727752a3b84d394a49237c3d
HASHEBUR RAHMAN <you@example.com>
2023-08-15 08:15:29 +0600

    :white_check_mark: update header

--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -2,12 +2,12 @@ import { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 class HeaderOne extends Component {
-    render() { 
-        window.addEventListener('scroll', function() {
+    render() {
+        window.addEventListener('scroll', function () {
             var value = window.scrollY;
             if (value > 200) {
                 document.querySelector('.header').classList.add(['header-fixed'], ['animated'], ['fadeInDown'])
-            }else{
+            } else {
                 document.querySelector('.header').classList.remove(['header-fixed'], ['animated'], ['fadeInDown'])
             }
         });
@@ -25,48 +25,21 @@ class HeaderOne extends Component {
                             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                                 <div className="navbar-nav mainmenu">
                                     <ul>
-                                        <li className="menu-item-has-children">
-                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-offset="0,0">Home</a>
-                                            <ul className="dropdown-menu">
-                                                <li><NavLink to="/" className={'active'}>Home Page One</NavLink></li>
-                                                <li><NavLink to="/index-2">Home Page Two</NavLink></li>
-                                                <li><NavLink to="/index-3">Home Page Three</NavLink></li>
-                                            </ul>
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-offset="0,0">Pages</a>
-                                            <ul className="dropdown-menu">
-                                                <li><NavLink to="/about">About Us</NavLink></li>
-                                                <li><NavLink to="/membership">Membership</NavLink></li>
-                                                <li><NavLink to="/comingsoon">comingsoon</NavLink></li>
-                                                <li><NavLink to="/404">404</NavLink></li>
-                                            </ul>
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-offset="0,0">Community</a>
-                                            <ul className="dropdown-menu">
-                                                <li><NavLink to="/community">Community</NavLink></li>
-                                                <li><NavLink to="/group">All Group</NavLink></li>
-                                                <li><NavLink to="/members">All Members</NavLink></li>
-                                                <li><NavLink to="/activity">Activity</NavLink></li>
+                                        <li>
 
-                                            </ul>
+                                            <Link to="/"> Home</Link>
+                                        </li>
+                                        <li >
+                                            <li><NavLink to="/about">About Us</NavLink></li>
+                                        </li>
+                                        <li>
+                                            <li><NavLink to="/community">Community</NavLink></li>
+                                        </li>
+                                        <li >
+                                            <li><NavLink to="/shop">Product</NavLink></li>
                                         </li>
                                         <li className="menu-item-has-children">
-                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-offset="0,0">Shops</a>
-                                            <ul className="dropdown-menu">
-                                                <li><NavLink to="/shop">Product</NavLink></li>
-                                                <li><NavLink to="/shop-single">Product Details</NavLink></li>
-                                                <li><NavLink to="/shop-cart">Product Cart</NavLink></li>
-                                            </ul>
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-offset="0,0">Blogs</a>
-                                            <ul className="dropdown-menu">
-                                                <li><NavLink to="/blog">Blog</NavLink></li>
-                                                <li><NavLink to="/blog-2">Blog Style Two</NavLink></li>
-                                                <li><NavLink to="/blog-single">Blog Details</NavLink></li>
-                                            </ul>
+                                        <li><NavLink to="/blog">Blog</NavLink></li>
                                         </li>
                                         <li><NavLink to="/contact">contact</NavLink></li>
                                     </ul>
@@ -86,5 +59,5 @@ class HeaderOne extends Component {
         );
     }
 }
- 
+
 export default HeaderOne;
